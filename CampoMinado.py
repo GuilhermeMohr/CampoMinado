38912141c8eeb425fc785cb1c6fccd3932ae8fb7
GuilhermeMohr <72572592+GuilhermeMohr@users.noreply.github.com>
2023-09-15 14:00:11 -0300

    Campo Minado Funcional

--- CampoMinado.py
+++ CampoMinado.py
@@ -1,3 +1,144 @@
-mapa = open("map.txt", 'r')
+import EditorMapa
+import os
 
-mapa.close()
+mapa = {"arquivo":open("map.txt", 'r'), 'x':0, 'y':0, "coordenadas":[], "display":[], "vazio":0}
+
+def inicializar():
+    i = mapa["arquivo"].readlines()
+
+    ii = i[-3].split('=')
+    mapa["x"] = int(ii[1])
+    del i[-3]
+ 
+    ii = i[-2].split('=')
+    mapa["y"] = int(ii[1])
+    del i[-2]
+
+    ii = i[-1].split('=')
+    mapa["vazio"] = int(ii[1])
+    del i[-1]
+ 
+    for linha in i:
+        i = linha.split(',')
+        ii = []
+        iii = []
+        
+        for ponto in i:
+            if ponto != '\n':
+                ii.append(ponto)
+                iii.append('#')
+        mapa["coordenadas"].append(ii)
+        mapa["display"].append(iii)
+       
+    mapa["arquivo"].close()
+    
+def revelar(y, x):
+    i = 0
+    if y+1 < mapa['y']:
+        if mapa["coordenadas"][y+1][x] == '*':
+            i+=1
+        if mapa["coordenadas"][y+1][x-1] == '*' and x-1 >= 0:
+            i+=1
+
+    if x+1 < mapa['x']:
+        if mapa["coordenadas"][y][x+1] == '*':
+            i+=1
+        if mapa["coordenadas"][y-1][x+1] == '*' and y-1 >= 0:
+            i+=1
+    
+    if y+1 < mapa['y'] and x+1 < mapa['x']:
+        if mapa["coordenadas"][y+1][x+1] == '*':
+            i+=1
+
+    if mapa["coordenadas"][y-1][x] == '*' and y-1 >= 0:
+        i+=1
+    if mapa["coordenadas"][y][x-1] == '*' and x-1 >= 0:
+        i+=1
+    if mapa["coordenadas"][y-1][x-1] == '*' and y-1 >= 0 and x-1 >= 0:
+        i+=1
+    
+    if i == 0:
+        if mapa["display"][y][x] == '#':
+            mapa["display"][y][x] = '-'
+            mapa["vazio"] -= 1
+        
+        if y+1 < mapa['y']:
+            if mapa["display"][y+1][x] == '#':
+                revelar(y+1, x)
+            if mapa["display"][y+1][x-1] == '#' and x-1 >= 0:
+                revelar(y+1, x-1)
+                
+        if x+1 < mapa['x']:
+            if mapa["display"][y][x+1] == '#':
+                revelar(y, x+1)
+            if mapa["display"][y-1][x+1] == '#' and y-1 >= 0:
+                revelar(y-1, x+1)
+                
+        if y+1 < mapa['y'] and x+1 < mapa['x']:
+            if mapa["display"][y+1][x+1] == '#':
+                revelar(y+1, x+1)
+
+        if mapa["display"][y-1][x] == '#' and y-1 >= 0:
+            revelar(y-1, x)
+        if mapa["display"][y][x-1] == '#' and x-1 >= 0:
+            revelar(y, x-1)
+        if mapa["display"][y-1][x-1] == '#' and y-1 >= 0 and x-1 >= 0:
+            revelar(y-1, x-1)
+        
+    else:
+        if mapa["display"][y][x] == '#':
+            mapa["display"][y][x] = i
+            mapa["vazio"] -= 1
+        
+ 
+inicializar()
+
+win = False
+
+while(True):
+    if mapa["vazio"] == 0:
+        win = True
+        break;
+
+    os.system("cls")
+    for coluna in mapa["display"]:
+        for ponto in coluna:
+            print(ponto, end='')
+        print()    
+    print(mapa["vazio"])
+
+    x = 0
+    y = 0
+    while(True):
+        print("x:")
+        x = int(input()) -1
+        print("y:")
+        y = int(input()) -1
+     
+        if x < mapa['x'] and y < mapa['y'] and x >= 0 and y >= 0:
+            break;
+        else:
+            print("Ocorreu um erro, repita o preocesso.")
+        
+
+    if mapa["coordenadas"][y][x] == '*':
+        mapa["display"][y][x] = '*'
+        break;
+    else:
+        revelar(y, x)
+        
+os.system("cls")
+if win:
+    print("_____.___.               __      __                    ")
+    print("\\__  |   | ____  __ __  /  \\    /  \\____   ____     ")
+    print(" /   |   |/  _ \\|  |  \\ \\   \\/\\/   /  _ \\ /    \\    ")
+    print(" \\____   (  <_> )  |  /  \\        (  <_> )   |  \\ ")
+    print(" / ______|\\____/|____/    \\__/\\  / \\____/|___|  /  ")
+    print(" \\/                            \\/             \\/    ")
+else:
+    print("  ________                        ________                         ")
+    print(" / _____ / _____    _____   ____   \\_____  \\___   __ ___________ ")
+    print("/   \\  ___\\__   \\  /     \\_/ __ \\   /   |   \\  \\ / // __ \\_  __ \\     ")
+    print("\\    \\_\\  \\/ __  \\|  Y Y  \\  ___/  /    |    \\    /\\  ___/|  | \\/")
+    print(" \\______  (____  / __|_|  /\__  >  \______  / \\_ /  \\___  >__|    ")
+    print("        \\/     \\/       \\/    \\/          \\/           \\/        ")
--- EditorMapa.py
+++ EditorMapa.py
@@ -1,18 +1,19 @@
 import random
 
 mapa = open("map.txt", 'w')
-map = {'x':10, 'y':10}
-
-mapa.write("Mapa x=10, y=10\n")
+map = {'x':10, 'y':10, "vazio":0}
 
 random.seed()
 
 for i in range(map['x']):
     for i in range(map['y']):
-        if(random.getrandbits(2)>2):
-            mapa.write('*')
+        if(random.getrandbits(3)>6):
+            mapa.write('*,')
         else:
-            mapa.write('#')
+            map["vazio"] += 1
+            mapa.write('#,')
     mapa.write("\n")
+    
+mapa.write(f"Mapa x={map['x']}\ny={map['y']}\nVazio={map['vazio']}")
 
 mapa.close()
